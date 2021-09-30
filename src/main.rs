bc3a2e8a5cd68807d7eb50ff04648caad709eb49
Yedaya <yedaya.ka@gmail.com>
2021-09-30 22:56:24 +0300

    Initial commit of torrent parser
    Implemented basic BEncoding parsing from files, without reading the
    files entirely into memory.
    This doesn't work on actual torrent files yet because of ut8 encoding
    issues

---
+++ src/main.rs
@@ -0,0 +1,264 @@
+#[macro_use]
+extern crate error_chain;
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader};
+use std::path::PathBuf;
+use structopt::StructOpt;
+use log::{debug, error, info, log, trace};
+use utf8_bufread::{BufRead, CharIter, Error};
+
+mod errors {
+    error_chain! {
+        errors {
+            CompletedReader {
+                description("At the end of the reader, nothing to read")
+                display("Nothing to read")
+            }
+            Error
+        }
+    }
+}
+use errors::*;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u8,
+}
+
+#[derive(Debug)]
+enum Bencoded {
+    Integer(i64),
+    String(String),
+    List(Vec<Bencoded>),
+    Dictionary(HashMap<String, Bencoded>),
+}
+
+fn main() {
+    env_logger::init();
+    if let Err(ref e) = run() {
+        error!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            error!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            error!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let args = Cli::from_args();
+    debug!("Reading file {:?}, Verbosity: {}", args.file, args.verbose);
+    let file = File::open(args.file).chain_err(|| "Couldn't open file")?;
+    let reader = BufReader::new(file);
+    parse_torrent(reader)?;
+    Ok(())
+}
+
+fn parse_torrent(mut reader: impl BufRead) -> Result<()> {
+    let mut buf_reader = BufReader::new(reader);
+    let mut reader = buf_reader.char_iter().peekable();
+    loop {
+        let bencoded = read_bencoded(&mut reader).chain_err(|| "Failed to parse bencoded")?;
+        debug!("{:?}", bencoded)
+    }
+}
+
+fn read_bencoded(
+    reader: &mut std::iter::Peekable<CharIter<BufReader<impl BufRead>>>,
+) -> Result<Bencoded> {
+    // This parsing is based on this spec: https://wiki.theory.org/BitTorrentSpecification
+
+    let first_ch = get_char(reader)?;
+    // Strings start with a number signifying it's length.
+    // For example '4:rick' is the string "rick" with length 4
+    if first_ch.is_ascii_digit() {
+        debug!("Found a digit, detected a string");
+        let mut string_length = String::new();
+        string_length.push(first_ch);
+        string_length.push_str(read_until(reader, ':')?.as_str());
+        let length = string_length
+            .parse::<usize>()
+            .chain_err(|| "couldn't parse number")?;
+        debug!("Reading {} characters", string_length);
+        let mut actual_string = String::with_capacity(length);
+        for _ in 0..length {
+            actual_string.push(get_char(reader)?);
+        }
+        debug!("Found a string {:?}", actual_string);
+        return Ok(Bencoded::String(actual_string));
+    // Numbers are in this format: 'ixxe', for example i456e is the number 456.
+    } else if first_ch == 'i' {
+        let mut number_string = String::new();
+        let mut digit_count = 0;
+        loop {
+            // Stop if number is more than 64 bit
+            if digit_count > 20 {
+                bail!("Too large numbers (> 64 bit)")
+            }
+            let ch = get_char(reader)?;
+            if digit_count >= 1 && ch == 'e' {
+                let number = number_string
+                    .parse::<i64>()
+                    .chain_err(|| "Couldnt convert to number")?;
+                return Ok(Bencoded::Integer(number));
+            }
+            if !ch.is_ascii_digit() {
+                bail!("Invalid file: non digits in a number");
+            }
+            number_string.push(ch);
+            digit_count += 1;
+            if digit_count > 1 && number_string.starts_with('0') {
+                bail!("Invalid number: No leading zeroes allowd");
+            }
+        }
+    }
+    // Lists start with l and enf with an e
+    else if first_ch == 'l' {
+        debug!("Found list");
+        let mut items = Vec::<Bencoded>::new();
+        let mut current_item: Result<Bencoded>;
+        loop {
+            if peek_char(reader)? == 'e' {
+                reader.next();
+                return Ok(Bencoded::List(items));
+            }
+            current_item = read_bencoded(reader);
+            match current_item {
+                Err(err) => match err {
+                    errors::Error(errors::ErrorKind::CompletedReader, _) => {
+                        return Ok(Bencoded::List(items));
+                    }
+                    errors::Error(_, _) => {
+                        return Err(err);
+                    }
+                },
+                Ok(value) => {
+                    debug!("Found list item {:?}", value);
+                    items.push(value);
+                }
+            }
+        }
+    }
+    // This is a dictionary
+    else if first_ch == 'd' {
+        debug!("Found dictionary");
+        let mut dict = HashMap::new();
+        loop {
+            if peek_char(reader)? == 'e' {
+                reader.next();
+                return Ok(Bencoded::Dictionary(dict));
+            }
+            debug!("Trying to read key");
+            let bencoded_key = read_bencoded(reader)?;
+            if let Bencoded::String(key) = bencoded_key {
+                debug!("Found key \"{}\", reading value", key);
+                let value = read_bencoded(reader)?;
+                debug!("Found value {:?}. Inserting...", value);
+                dict.insert(key, value);
+            } else {
+                bail!("Only Strings are can be keys in becoded dictionaries")
+            }
+        }
+    }
+    Ok(Bencoded::String("".to_string()))
+}
+
+fn get_char(reader: &mut std::iter::Peekable<CharIter<BufReader<impl BufRead>>>) -> Result<char> {
+    let ch = reader.next();
+    match ch {
+        Some(ch) => {
+            let ch = ch.chain_err(|| "")?;
+            if ch == '\0' {
+                Err(errors::ErrorKind::CompletedReader.into())
+            } else {
+                Ok(ch)
+            }
+        }
+        None => Err(errors::ErrorKind::CompletedReader.into()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    use utf8_bufread::BufRead;
+
+    use crate::get_char;
+
+    #[test]
+    fn reading_one_ascii_char() {
+        // create_reader!("First");
+        let mut st = BufReader::new("First".as_bytes());
+        let mut peek_reader = st.char_iter().peekable();
+        let ch = get_char(&mut peek_reader).unwrap();
+        assert_eq!(ch, 'F');
+    }
+
+    #[test]
+    fn utf_char() {
+        let mut reader = BufReader::new("שלום".as_bytes());
+        let mut peek_reader = reader.char_iter().peekable();
+        let ch = get_char(&mut peek_reader).expect("");
+        assert_eq!(ch, 'ש');
+    }
+
+    #[test]
+    fn chars_in_order() {
+        let mut reader = BufReader::new("First".as_bytes());
+        let mut peek_reader = reader.char_iter().peekable();
+        let _first_char = get_char(&mut peek_reader).unwrap();
+        let second_char = get_char(&mut peek_reader).unwrap();
+        assert_eq!(second_char, 'i');
+    }
+}
+
+fn peek_char(reader: &mut std::iter::Peekable<CharIter<BufReader<impl BufRead>>>) -> Result<char> {
+    let ch_opt = reader.peek();
+    match ch_opt {
+        Some(ch) => {
+            match ch {
+                Err(error) => {
+                    bail!("Failed to read char: {}", error.to_string());
+                    // Err(error.into())
+                }
+                Ok(ch) => {
+                    if *ch == '\0' {
+                        Err(errors::ErrorKind::CompletedReader.into())
+                    } else {
+                        Ok(*ch)
+                    }
+                }
+            }
+            // let ch = *ch.as_ref()?;
+        }
+        None => Err(errors::ErrorKind::CompletedReader.into()),
+    }
+}
+
+fn read_until(
+    reader: &mut std::iter::Peekable<CharIter<BufReader<impl BufRead>>>,
+    ch: char,
+) -> Result<String> {
+    let mut str = String::new();
+    let mut current_char: char;
+    loop {
+        current_char = get_char(reader)?;
+        if current_char == ch {
+            return Ok(str);
+        }
+        str.push(current_char);
+    }
+}
